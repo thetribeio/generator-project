50d4314c78c5323af6f3b9f5e5a470da0605ba85
Jérôme Vasseur <jerome.vasseur@thetribe.io>
2020-12-02 16:56:52 +0100

    Add more files to app and express generators

--- generators/express/index.ts
+++ generators/express/index.ts
@@ -2,17 +2,20 @@ import BaseGenerator from '../../utils/BaseGenerator';
 
 class NodeGenerator extends BaseGenerator {
     writing() {
+        const nodeVersion = '14.15.1';
         const { name } = this.options;
 
         this.fs.copyTpl(
             this.templatePath('base'),
             this.destinationPath(name),
-            { name },
+            { name, nodeVersion },
+            undefined,
+            { globOptions: { dot: true } },
         );
 
         this.configureDockerCompose(this.templatePath('docker-compose.yaml.ejs'), { name });
 
-        this.configureCircleCI(this.templatePath('circleci.yaml.ejs'), { name });
+        this.configureCircleCI(this.templatePath('circleci.yaml.ejs'), { name, nodeVersion });
     }
 }
 
