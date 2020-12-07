11d2b649bfa4566df6a0af8516fac7acef9f6ec9
Jérôme Vasseur <jerome.vasseur@thetribe.io>
2020-12-07 13:59:04 +0100

    Simplify NodeJS version handling

--- generators/express/index.ts
+++ generators/express/index.ts
@@ -2,20 +2,19 @@ import BaseGenerator from '../../utils/BaseGenerator';
 
 class NodeGenerator extends BaseGenerator {
     writing() {
-        const nodeVersion = '14.15.1';
         const { name } = this.options;
 
         this.fs.copyTpl(
             this.templatePath('base'),
             this.destinationPath(name),
-            { name, nodeVersion },
+            { name },
             undefined,
             { globOptions: { dot: true } },
         );
 
         this.configureDockerCompose(this.templatePath('docker-compose.yaml.ejs'), { name });
 
-        this.configureCircleCI(this.templatePath('circleci.yaml.ejs'), { name, nodeVersion });
+        this.configureCircleCI(this.templatePath('circleci.yaml.ejs'), { name });
     }
 }
 
