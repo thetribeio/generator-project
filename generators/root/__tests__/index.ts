862416833298dc45164af6dbd49671fb07b1a402
Jérôme Vasseur <jerome.vasseur@thetribe.io>
2020-12-08 12:16:59 +0100

    Import ansible-skeleton-kickstart into the root generator

---
+++ generators/root/__tests__/index.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import helpers from 'yeoman-test';
+
+describe('When running the generator', () => {
+    let root: string;
+
+    beforeAll(async () => {
+        root = await helpers.run(path.resolve(__dirname, '..'));
+    });
+
+    afterAll(async () => {
+        await fs.promises.rm(root, { recursive: true });
+    });
+
+    test('It generates an ansible configuration', async () => {
+        await fs.promises.access(path.join(root, 'ansible', 'staging'), fs.constants.R_OK);
+    });
+});
