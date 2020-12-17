a70814732ff8d755c475791640ffcdc837291a71
Jérôme Vasseur <jerome.vasseur@thetribe.io>
2020-12-17 12:03:40 +0100

    Add test for running app generator with Next.js

--- generators/app/__tests__/index.ts
+++ generators/app/__tests__/index.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import helpers from 'yeoman-test';
 
-describe('When running the generator with create-react-app', () => {
+describe('When running the generator with Create React App', () => {
     let root: string;
 
     beforeAll(async () => {
@@ -14,9 +14,34 @@ describe('When running the generator with create-react-app', () => {
         await fs.promises.rm(root, { recursive: true });
     });
 
-    test('It generates an express backend', async () => {
+    test('It generates an Express backend', async () => {
         const config = JSON.parse(await fs.promises.readFile(path.resolve(root, 'backend', 'package.json'), 'utf8'));
 
         expect(config.dependencies.express).toBeDefined();
+    });
+});
+
+describe('When running the generator with Next.js', () => {
+    let root: string;
+
+    beforeAll(async () => {
+        root = await helpers.run(path.resolve(__dirname, '..'))
+            .withPrompts({ frontend: 'next-js' });
+    });
+
+    afterAll(async () => {
+        await fs.promises.rm(root, { recursive: true });
+    });
+
+    test('It generates an Express backend', async () => {
+        const config = JSON.parse(await fs.promises.readFile(path.resolve(root, 'backend', 'package.json'), 'utf8'));
+
+        expect(config.dependencies.express).toBeDefined();
+    });
+
+    test('It generates an Next.js frontend', async () => {
+        const config = JSON.parse(await fs.promises.readFile(path.resolve(root, 'frontend', 'package.json'), 'utf8'));
+
+        expect(config.dependencies.next).toBeDefined();
     });
 });
--- generators/next-js/index.ts
+++ generators/next-js/index.ts
@@ -1,6 +1,12 @@
-import Generator from 'yeoman-generator';
+import Generator, { GeneratorOptions } from 'yeoman-generator';
 
 class NextJSGenerator extends Generator {
+    constructor(args: string | string[], opts: GeneratorOptions) {
+        super(args, opts);
+
+        this.argument('name', { type: String, required: true });
+    }
+
     writing() {
         const { name } = this.options;
         this.fs.copyTpl(this.templatePath('base'), this.destinationPath(name), {
