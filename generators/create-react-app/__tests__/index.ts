2ed01d25c9859bc7bc1bff02b9d7b5873cbcf44d
Jérôme Vasseur <jerome.vasseur@thetribe.io>
2021-01-04 17:25:20 +0100

    Import CRA template

---
+++ generators/create-react-app/__tests__/index.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+import helpers from 'yeoman-test';
+import run from '../../../utils/run';
+
+describe('When running the generator', () => {
+    let root: string;
+
+    beforeAll(async () => {
+        root = await helpers.run(path.resolve(__dirname, '../../root'))
+            .withPrompts({ contactEmail: 'test@example.com' });
+
+        await helpers.run(path.resolve(__dirname, '..'))
+            .cd(root)
+            .withOptions({ skipInstall: false })
+            .withArguments(['test']);
+    });
+
+    afterAll(async () => {
+        await fs.promises.rm(root, { recursive: true });
+    });
+
+    test('It generates a project which correctly builds', async () => {
+        await run('yarn', ['build'], { cwd: path.resolve(root, 'test') });
+    });
+});
